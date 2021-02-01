6629e441aa1b703ea056681e40179c4fec4de1ad
tanwer <740028600@qq.com>
2021-02-01 11:42:45 +0800

    init

---
+++ assets/tongji.js
@@ -0,0 +1,14 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  //hm.src = "https://hm.baidu.com/hm.js?0ec146f4cc456fb4f1242e89446eca86";
+  hm.src = "https://hm.baidu.com/hm.js?b09da2145e094547063efbd8d4b30b1d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+// cnzz 统计
+document.write(unescape("%3Cspan id='cnzz_stat_icon_1274949385'%3E%3C/span%3E%3Cscript src='https://s13.cnzz.com/z_stat.php%3Fid%3D1274949385' type='text/javascript'%3E%3C/script%3E"));
+setTimeout(function(){
+  var ele = document.getElementById("cnzz_stat_icon_1274949385");
+  ele.style.display = 'none';
+}, 1000)
